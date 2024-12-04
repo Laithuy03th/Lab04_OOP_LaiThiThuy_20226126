d4fe06c35f4e187ed5cb2d8a002c7af22e5249ca
Laithuy03th <laithuy03th@gmail.com>
2024-12-04 08:12:21 +0700

    lab 4

--- AimsProject_Lab04_3/src/hust/soict/dsai/aims/aims/Aims/Aims.java
+++ AimsProject_Lab04_3/src/hust/soict/dsai/aims/aims/Aims/Aims.java
@@ -1,26 +1,28 @@
-package hust.soict.dsai.aims.aims.Aims;
+package hust.soict.dsai.aims.aims;
+
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
 public class Aims {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Cart anOrder = new Cart();
+                Cart anOrder = new Cart();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-                "Animation", "Roger Allers", 87, 19.95f);
-        anOrder.addDigitalVideoDisc(dvd1);
+                DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                                "Animation", "Roger Allers", 87, 19.95f);
+                anOrder.addDigitalVideoDisc(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
-                "Science Fiction", "George Lucas", 87, 24.95f);
-        anOrder.addDigitalVideoDisc(dvd2);
+                DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                                "Science Fiction", "George Lucas", 87, 24.95f);
+                anOrder.addDigitalVideoDisc(dvd2);
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin",
-                "Animation", 18.99f);
-        anOrder.addDigitalVideoDisc(dvd3);
+                DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin",
+                                "Animation", 18.99f);
+                anOrder.addDigitalVideoDisc(dvd3);
 
-        System.out.println("Total Cost is: ");
-        System.out.println(anOrder.totalCost());
+                System.out.println("Total Cost is: ");
+                System.out.println(anOrder.totalCost());
 
-        anOrder.removeDigitalVideoDisc(dvd2);
-    }
+                anOrder.removeDigitalVideoDisc(dvd2);
+        }
 }
